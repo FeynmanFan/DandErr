2286e7688a87a140fca487dd9afee63902907727
Chris B. Behrens <cbehrens@gmail.com>
2022-01-11 11:33:42 -0600

    Removed "unnecessary" parser initializer

--- DandE.DocumentHandler/WordDocumentCard.cs
+++ DandE.DocumentHandler/WordDocumentCard.cs
@@ -12,11 +12,6 @@
         {
             get
             {
-                if (this.parser == null)
-                {
-                    this.parser = new WordDocumentParser();
-                }
-
                 return this.parser;
             }
         }
