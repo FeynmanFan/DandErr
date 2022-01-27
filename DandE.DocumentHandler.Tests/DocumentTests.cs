ef33a734ece4abd1814cf31c403f9648a746f4f8
Chris B. Behrens <cbehrens@gmail.com>
2022-01-27 14:07:29 -0600

    Added current directory

--- DandE.DocumentHandler.Tests/DocumentTests.cs
+++ DandE.DocumentHandler.Tests/DocumentTests.cs
@@ -2,6 +2,7 @@ namespace DandE.DocumentHandler.Tests
 {
     using Xunit;
     using System.IO;
+    using System.Diagnostics;
 
     public class DocumentTests
     {
@@ -23,6 +24,8 @@ namespace DandE.DocumentHandler.Tests
         [Fact]
         public void OutputFileFunctionProducesFile()
         {
+            Debug.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
+
             var filePath = @"/home/nt-user/workspace/DandErr/outputfile.txt";
 
             Assert.True(File.Exists(filePath), "$File does not exist at {filePath}");
