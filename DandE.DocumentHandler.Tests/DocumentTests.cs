3cfb059795de778348ddc078de4af2c953bee7e7
Chris B. Behrens <cbehrens@gmail.com>
2022-01-27 14:11:01 -0600

    Removed test

--- DandE.DocumentHandler.Tests/DocumentTests.cs
+++ DandE.DocumentHandler.Tests/DocumentTests.cs
@@ -20,19 +20,5 @@ namespace DandE.DocumentHandler.Tests
             Assert.Equal(expectedText, docCard.Text);
             Assert.Equal(expectedTitle, docCard.Title);
         }
-
-        [Fact]
-        public void OutputFileFunctionProducesFile()
-        {
-            Debug.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
-
-            var filePath = @"/home/nt-user/workspace/DandErr/outputfile.txt";
-
-            Assert.True(File.Exists(filePath), "$File does not exist at {filePath}");
-
-            var result = File.ReadAllText(filePath);
-
-            Assert.True(!string.IsNullOrEmpty(result), "File existed, but is empty");
-        }
     }
 }
