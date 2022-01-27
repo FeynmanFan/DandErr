7d1c03e88378204f397a17ea83bcaa69d34a9e51
Chris B. Behrens <cbehrens@gmail.com>
2022-01-27 14:13:28 -0600

    Removed other test

--- DandE.DocumentHandler.Tests/DocumentTests.cs
+++ DandE.DocumentHandler.Tests/DocumentTests.cs
@@ -7,18 +7,17 @@ namespace DandE.DocumentHandler.Tests
     public class DocumentTests
     {
         [Fact]
-        public void Doc1Properties()
+        public void OutputFileFunctionProducesFile()
         {
-            // arrange
-            var expectedText = "Test Document #1To sit in solemn silence in a dull, dark, dock, In a pestilential prison, with a life-long lock,Awaiting the sensation of a short, sharp, shock, From a cheap and chippy chopper on a big black block";
-            var expectedTitle = "Test Document #1";
+            Debug.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
 
-            // act
-            var docCard = new WordDocumentCard(TestFiles.Test_Document);
+            var filePath = @"/home/nt-user/workspace/DandErr/outputfile.txt";
 
-            // assert
-            Assert.Equal(expectedText, docCard.Text);
-            Assert.Equal(expectedTitle, docCard.Title);
+            Assert.True(File.Exists(filePath), "$File does not exist at {filePath}");
+
+            var result = File.ReadAllText(filePath);
+
+            Assert.True(!string.IsNullOrEmpty(result), "File existed, but is empty");
         }
     }
 }
