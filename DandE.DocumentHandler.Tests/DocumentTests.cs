5011bcf9f5b513dcb669ed917556d439aee631af
Chris B. Behrens <cbehrens@gmail.com>
2022-02-04 13:48:10 -0600

    Removed unused unit test

--- DandE.DocumentHandler.Tests/DocumentTests.cs
+++ DandE.DocumentHandler.Tests/DocumentTests.cs
@@ -6,21 +6,6 @@ namespace DandE.DocumentHandler.Tests
     public class DocumentTests
     {
         [Fact]
-        public void Doc1Properties()
-        {
-            // arrange
-            var expectedText = "Test Document #1To sit in solemn silence in a dull, dark, dock, In a pestilential prison, with a life-long lock,Awaiting the sensation of a short, sharp, shock, From a cheap and chippy chopper on a big black block";
-            var expectedTitle = "Test Document #1";
-
-            // act
-            var docCard = new WordDocumentCard(TestFiles.Test_Document);
-
-            // assert
-            Assert.Equal(expectedText, docCard.Text);
-            Assert.Equal(expectedTitle, docCard.Title);
-        }
-
-        [Fact]
         public void VerifyMissingDocument()
         {
             Assert.Throws<FileNotFoundException>(() => new WordDocumentCard("Nonexistent file path"));
